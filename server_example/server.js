9f5462e334f8adc2dd942cc8db96f323fa75b76c
Juuzu99 <127954058+Juuzu99@users.noreply.github.com>
2023-04-03 19:06:21 +0300

    mysql+test

--- server_example/server.js
+++ server_example/server.js
@@ -1,3 +1,7 @@
+const mysql = require('mysql');
+const session = require('express-session');
+const path = require('path');
+
 // Load required modules
 var http    = require("http");              // http server core module
 var express = require("express");           // web framework external module
@@ -10,6 +14,12 @@ var socketIo = require("socket.io");        // web socket external module
 // 2. install easyrtc (npm i open-easyrtc --save) in server_example/package.json
 
 var easyrtc = require("../"); // EasyRTC internal module
+const connection = mysql.createConnection({
+	host     : 'localhost',
+	user     : 'root',
+	password : '135246Aleks',
+	database : 'nodelogin'
+});
 
 // Set process name
 process.title = "node-easyrtc";
@@ -18,6 +28,61 @@ process.title = "node-easyrtc";
 var app = express();
 app.use(serveStatic('static', {'index': ['index.html']}));
 
+app.use(session({
+	secret: 'secret',
+	resave: true,
+	saveUninitialized: true
+}));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'static')));
+
+// http://localhost:3000/
+app.get('/', function(request, response) {
+	// Render login template
+	response.sendFile(path.join(__dirname + './static/login.html'));
+});
+
+// http://localhost:3000/auth
+app.post('/auth', function(request, response) {
+	// Capture the input fields
+	let username = request.body.username;
+	let password = request.body.password;
+	// Ensure the input fields exists and are not empty
+	if (username && password) {
+		// Execute SQL query that'll select the account from the database based on the specified username and password
+		connection.query('SELECT * FROM accounts WHERE username = ? AND password = ?', [username, password], function(error, results, fields) {
+			// If there is an issue with the query, output the error
+			if (error) throw error;
+			// If the account exists
+			if (results.length > 0) {
+				// Authenticate the user
+				request.session.loggedin = true;
+				request.session.username = username;
+				// Redirect to home page
+				response.redirect('/home');
+			} else {
+				response.send('Incorrect Username and/or Password!');
+			}			
+			response.end();
+		});
+	} else {
+		response.send('Please enter Username and Password!');
+		response.end();
+	}
+});
+// http://localhost:3000/home
+app.get('/home', function(request, response) {
+	// If the user is loggedin
+	if (request.session.loggedin) {
+		// Output username
+		response.send('Welcome back, ' + request.session.username + '!');
+	} else {
+		// Not logged in
+		response.send('Please login to view this page!');
+	}
+	response.end();
+});
 // Start Express http server on port 8080
 var webServer = http.createServer(app);
 
